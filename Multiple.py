dabb5e7e9a18465d8de6a513fcfffb5af6feb756
aayu-narwariya07 <aartinarwariya26@gmail.com>
2025-06-09 13:55:11 +0530

    Create Multiple.py

---
+++ Multiple.py
@@ -0,0 +1,14 @@
+# input marks
+marks = int(input("Enter your marks: "))
+
+# if-elif-else statements
+if marks >=90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+elif marks >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
