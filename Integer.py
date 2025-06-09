2570ac8a65da090bbe3e0eb90f8be1798c20351e
aayu-narwariya07 <aartinarwariya26@gmail.com>
2025-06-09 13:59:44 +0530

    Create Integer.py

---
+++ Integer.py
@@ -0,0 +1,8 @@
+#1 Two integers sum
+num1 = int(input("Enter the frist integer "))
+num2 = int(input("Entern the second integer "))
+sum_of_numbers = num1 + num2
+if 10 <= sum_of_numbers <= 20:
+    print(15)
+else:
+    print(0)
