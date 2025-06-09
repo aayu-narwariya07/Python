31ff9258bd0bcbf0ceb5d91e4f34cd2e589d86b1
aayu-narwariya07 <aartinarwariya26@gmail.com>
2025-06-09 13:53:02 +0530

    Create ComplaexCondition.py

---
+++ ComplaexCondition.py
@@ -0,0 +1,11 @@
+# Input numbers
+number = int(input("Enter a number: "))
+if number >= 0:
+    if number == 0:
+        print(F"The number is zero.: ")
+    else:
+      print(F"{number} is posotive. ")
+else:
+   print(F"{number} is negative. ")
+       
+    
