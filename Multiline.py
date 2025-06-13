4adf2135bfc8e8d17b2588891a825c0d04e333af
aayu-narwariya07 <aartinarwariya26@gmail.com>
2025-06-13 16:43:13 +0530

    Create Multiline.py

---
+++ Multiline.py
@@ -0,0 +1,108 @@
+# Basic multi-line print using 
+print("""This is an example of multi-linetext.
+      It is written over several lines,
+      and each line will be printed exactly as it appears here.""")
+# Multi-line print with Escape Characters
+print("I am Aarti.\nmy father bhagvan narwariya\nmy mother savitri")
+#combining Single and Multi-line print 
+print("Starting the process...\n")
+print("""Step 1: Data collection complete.
+      Step 2: Data processing is ongoing.
+    Step 3: Data output generation..."""  )
+print("process completed successfully!")
+print (f"The result of {15} +{4} is:{15 + 4 }")
+print(f"The result of {15}-{4} is: {15-4}")
+print(f"The result of {15} *{4} is:{15 *4}")
+print(f"The result of {15} /{4} is: {15/4}")
+print(f"The result of {15} **{4} is: {15**4}")
+print(f"The result of {15} //{4} is: {15//4}")
+# Inserting space between output using commas
+print("The sum of 5 and 3 is",5+3)
+print("The product of 6 and 8 is",6*8)
+# Custom separator between outputs
+print("5","+","3","=",5+3, sep="")
+print("4","/","4","=",4/4, sep="")
+# Inserting text at the end of a print statement using end parameter
+print("5+3 =",end="")
+print(5+3)
+print("4*7 =",end="")
+print(4*7)
+# This is a single-line comment
+x=10
+print(x)
+# Multi-line printing with multiple print statements
+print("Hello,World!")
+print("Welcome to python programming.")
+print("This is multi-lilne printing.")
+# multi-line printing with a single print statement
+print("Hello world!\nWelcome to python programming.\nThis is multi-line printing.")
+# This program calculates the area of a rectangle
+Length=10
+Width=5
+area = Length * Width
+print("The area of the rectangle is:",area)
+x = 10
+result=x*2
+print("The result is:",result)
+variable_name =10
+_variable_name =20
+name1 ="Edunet"
+_name2 ="Foundation"
+print(name1,_name2)
+variable1=5
+var_name_2=10
+varName3=15
+print(variable1,var_name_2,varName3)
+# case-sensitive veriable names 
+variable1="lowercase"
+variable="uppercase"
+VARIABLE ="ALL CAPS"
+print(variable1,variable,VARIABLE)
+var_name = 25
+var_name_2 = 30
+print(var_name,var_name_2)
+# Good descriptive variable names
+Student_name ="Edunet"
+total_score = 95
+is_registered =True
+print(Student_name,total_score,is_registered)
+# Assigning an integer value to a variable 
+age = 25
+height = 5.9
+name = "Edunet Foundation"
+is_student = True
+print(age,height,name,is_student)
+# multiple assignment
+a = b= c= 10
+x,y,Z= 1,2,3
+print(1,2,3)
+print(a,b,c)
+# Assigning integer literals to variables
+integer1 = 10
+integer2 =-5
+integer3 =0
+ # performing operation with integers
+sum_integers =integer1 + integer2
+product_integers =integer1 *3
+difference_integers =integer1 - integer2
+print("Integer values:")
+print("integer1 =",integer1)
+print("integer2 =",integer2)
+print("integer3 =",integer3)
+print("sum of integer1 and integer2:",sum_integers)
+print("product of integer1 and 3:",product_integers)
+print("Difference between integer1 and integer2:",difference_integers)
+# Assiging floating literals to variables
+float1 =3.14
+float2 = -2.718
+float3 = 0.0
+sum_floats =float1 + float2
+product_floats = float1 * 2
+difference_floats = float1 - float2
+print("\nFloating-point values:")
+print("float =",float1)
+print("float2 =",float2)
+print("float3 =",float3)
+print("sum of float1 and float2",sum_floats)
+print("product of float1 and 2:",product_floats)
+print("Difference between float1 and float2:",difference_floats)
