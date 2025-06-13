a7c71d0db425144472963173b20f1a3058ab4863
aayu-narwariya07 <aartinarwariya26@gmail.com>
2025-06-13 15:42:24 +0530

    Create MpdulesOpration.py

---
+++ MpdulesOpration.py
@@ -0,0 +1,13 @@
+# math modules
+import math
+# square root
+print("Square root of 25:", math.sqrt(25))
+
+# Factorial
+print("Factorial of 5:", math.factorial(5))
+
+# value of pi
+print("Value of pi:", math.pi)
+
+# cosine of 45 degrees
+print("cosine of 45 degree:", math.cos(math.radians(45)))
