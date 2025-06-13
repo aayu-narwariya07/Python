12dae550c070bfd635e064124c6742a57430cc9b
aayu-narwariya07 <aartinarwariya26@gmail.com>
2025-06-13 15:38:36 +0530

    Create importCalculate.py

---
+++ importCalculate.py
@@ -0,0 +1,10 @@
+# main.py
+import Calculater
+a = 10
+b = 5
+print("Addition:",Calculater.add(a,b))
+print("Subtraction:",Calculater.subtract(a,b))
+print("Multiplication:",Calculater.multiply(a,b))
+print("Division:",Calculater.divide(a,b))
+
+
