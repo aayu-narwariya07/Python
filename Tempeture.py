5426e823510e0e448e7e9147c96351ff26744fc1
aayu-narwariya07 <aartinarwariya26@gmail.com>
2025-06-13 16:47:23 +0530

    Create Tempeture.py

---
+++ Tempeture.py
@@ -0,0 +1,7 @@
+#  temperature
+temperature = int(input("Enter the temperature degree  "))
+degree = '°'
+if temperature >= 30:
+    print("the temperature is hot")
+else:
+    print("the temperature is cold ")
