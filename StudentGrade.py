e94ea6da151c0bff0cd4f366aa856ef909606fcf
aayu-narwariya07 <aartinarwariya26@gmail.com>
2025-06-13 16:46:45 +0530

    Create StudentGrade.py

---
+++ StudentGrade.py
@@ -0,0 +1,12 @@
+#  student marks
+student_grade = int(input("Enter your student grade  "))
+if student_grade >= 90:
+    print("grade A")
+elif student_grade >= 80:
+    print("grade B")
+elif student_grade >= 70:
+    print("grade C" )
+elif student_grade >= 60:
+    print("grade D ")
+else:
+    print("otherwise F")
